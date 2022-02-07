a0ed99af8adcacc2d643c5d57f85c9f7713d5eda
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-02-07 22:07:30 +0900

    setup componetns

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,6 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <div>
       <h1>Welcome to Remix</h1>
       <ul>
         <li>
